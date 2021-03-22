88dde7fa9ff4dd2d5e0f753e2cd9ebc736076fb4
sebastianpedraza2 <46971682+sebastianpedraza2@users.noreply.github.com>
2021-03-22 16:30:56 -0500

    Base commit

---
+++ app/src/main/java/com/example/messengerapp/ui/signin/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.messengerapp.ui.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.messengerapp.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
